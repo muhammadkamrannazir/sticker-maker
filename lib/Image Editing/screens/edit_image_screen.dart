d680762c7000c8a2598bea3169de97942bff2c3f
muhammadkamrannazir <bsf1802154@ue.edu.pk>
2023-10-04 08:27:53 +0500

    3

--- lib/Image Editing/screens/edit_image_screen.dart
+++ lib/Image Editing/screens/edit_image_screen.dart
@@ -497,7 +497,7 @@ class _EditImageScreenState extends EditImageViewModel {
               color: Colors.black,
             ),
           ),
-          //122
+          //1223
           FloatingActionButton.small(
             backgroundColor: Colors.white,
             onPressed: () {
